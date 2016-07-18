855fa40940c1aea4950e096f2addaec0bfbc5bb0
Changwan Jun <im@wani.kr>
2016-07-18 15:17:59 +0900

    add session to HttpServiceProvider @Wandu/Http

--- HttpServiceProvider.php
+++ HttpServiceProvider.php
@@ -1,9 +1,17 @@
 <?php
 namespace Wandu\Http;
 
+use Predis\Client;
+use SessionHandlerInterface;
 use Wandu\DI\ContainerInterface;
 use Wandu\DI\ServiceProviderInterface;
 use Wandu\Http\Psr\Factory\ResponseFactory;
+use Wandu\Http\Session\Handler\FileHandler;
+use Wandu\Http\Session\Handler\GlobalHandler;
+use Wandu\Http\Session\Handler\RedisHandler;
+use Wandu\Http\Session\SessionFactory;
+use function Wandu\Foundation\config;
+use function Wandu\Foundation\path;
 
 class HttpServiceProvider implements ServiceProviderInterface
 {
@@ -14,6 +22,23 @@ class HttpServiceProvider implements ServiceProviderInterface
     {
         $app->closure(ResponseFactory::class, function () {
             return response(); // singleton
+        });
+        $app->closure(SessionFactory::class, function ($app) {
+            return new SessionFactory($app[SessionHandlerInterface::class], [
+                'timeout' => config('session.timeout', 3600),
+                'name' => config('session.name', 'WdSessId'),
+                'gc_frequency' => config('session.gc_frequency', 100),
+            ]);
+        });
+        $app->closure(SessionHandlerInterface::class, function ($app) {
+            switch (config('session.type')) {
+                case 'file':
+                    return new FileHandler(path(config('session.path', 'cache/sessions')));
+                case 'redis':
+                    return new RedisHandler($app[Client::class], config('session.timeout', 3600));
+                default:
+                    return new GlobalHandler();
+            }
         });
     }
 
